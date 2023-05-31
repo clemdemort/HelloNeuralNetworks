06c96c76f59085c50830a99ea977f2a6151f325a
Clement Bartolone <bartoloneclement@gmail.com>
2023-05-31 15:50:38 +0200

    start of a neural network abstraction

--- main.c
+++ main.c
@@ -8,22 +8,75 @@
 
 typedef unsigned int u32;
 typedef struct neuron_s{
-	u32 wc;		//weight count
-	float *w;	//weights
-	float b;	//bias
+	//weight count
+	u32 wc;		
+	//weight list
+	float *w;	
+	//bias
+	float b;	
 }neuron_t;
-typedef neuron_t * neuron;
 
-neuron newNeuron(u32 wc){
-	neuron res = malloc(sizeof(neuron_t));
-	res->wc = wc;
-	res->w = calloc(sizeof(float) , wc);
+typedef struct layer_s{
+	//neuron count	
+	u32 nc;			
+	//neuron list
+	neuron_t * n;	
+}layer;
+
+typedef struct model_s{
+	//layer count	
+	u32 lc;			
+	//layer list
+	layer * l;	
+}model;
+
+typedef struct descriptor_s{
+	u32 * desc;
+	u32 descsize;
+}descriptor;
+
+//wc : weight count -> how many weights should each neuron have (e.g how many neurons/entries before)
+//nc : neuron count -> how many neurons in the layer
+layer newlayer(u32 wc,u32 nc){
+	layer res;
+	res.nc = nc;
+	res.n = malloc(sizeof(neuron_t)* nc);
+	for(u32 i = 0; i < nc; i++ ){
+		res.n[i].wc = wc;
+		res.n[i].w = calloc(sizeof(float) , wc);
+		res.n[i].b = 0;
+	}
 	return res;
 }
 
-void destroyNeuron(neuron n){
-	free(n->w);
-	free(n);
+void destroyLayer(layer l){
+	for(u32 i = 0; i < l.nc; i++ ){
+		free(l.n[i].w);
+	}
+	free(l.n);
+}
+
+
+model newModel(descriptor arch){
+	model res;
+	res.lc = arch.descsize;
+	res.l = malloc(sizeof(layer)*res.lc);
+	for(u32 i = 0; i < res.lc;i++){
+		//i=0 will be the entry layer and i = res.lc-1 will be the exit layer
+		//the rest (in between) will be hidden layers
+		u32 prevnc = 0;
+		if(i > 0)prevnc = arch.desc[i-1];
+		res.l[i] = newlayer(prevnc, arch.desc[i]);
+
+	}
+	return res;
+}
+
+void destroyModel(model m){
+	for(u32 i = 0; i < m.lc; i++){
+		destroyLayer(m.l[i]);
+	}
+	free(m.l);
 }
 
 typedef float cpl[3];
@@ -31,7 +84,7 @@ cpl data[] = {
 	{0,0,0},
 	{0,1,1},
 	{1,0,1},
-	{1,1,1},
+	{1,1,0},
 };
 #define data_count (sizeof(data)/sizeof(data[0]))
 
@@ -44,7 +97,7 @@ float sig(float x){
 float randfloat(){
 	return (float)rand()/(float)RAND_MAX;
 }
-
+/*
 float cost(neuron n){
 	float res = 0.0f;
 	for(size_t i = 0; i < data_count; i++){
@@ -65,13 +118,15 @@ void train(neuron n,float eps, float rate){
 	float * dw = malloc(n->wc*sizeof(float));
 	float c = cost(n);
 	for(u32 j = 0; j < n->wc;j++){
+		float save = n->w[j];
 		n->w[j] += eps;
 		dw[j] = rate * ((cost(n)-c)/eps);
-		n->w[j] -= eps;
+		n->w[j] = save;
 	}
+	float save  = n->b;
 	n->b += eps;
 	float db = rate * ((cost(n)-c)/eps);
-	n->b -= eps;
+	n->b = save;
 	n->b -= db;
 	for(u32 j = 0; j < n->wc;j++){
 		n->w[j] -= dw[j];
@@ -79,8 +134,9 @@ void train(neuron n,float eps, float rate){
 	free(dw);
 	//printf("cost : %f \tW1 : %f \tW2 : %f \tB: %f \n",cost(n),n->w[0],n->w[1],n->b);
 	//printf("%f\n",cost(w1,w2,b));
-}
+}*/
 int main(){
+	/*
 	srand(time(0));
 	neuron n = newNeuron(2);
 	//printf("w1 : %f, w2 : %f, B : %f , cost : %f\n",w1,w2,b,c);
@@ -109,7 +165,15 @@ int main(){
 		printf("actual : %f \texpected : %f\n",y,data[i][2]);
 
 	}
-	printf("cost : %f\n",cost(n));
-
+	printf("cost : %f\n",cost(n));*/
+	descriptor arch;
+	arch.descsize = 3;
+	arch.desc = malloc(sizeof(u32)*arch.descsize);
+	arch.desc[0] = 2;
+	arch.desc[1] = 2;
+	arch.desc[2] = 1;
+	model nn = newModel(arch);
+	destroyModel(nn);
+	free(arch.desc);
   	return EXIT_SUCCESS;
 }
